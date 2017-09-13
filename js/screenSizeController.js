6d60723c69ecfcc23635e8b2f7a890acdded8dfb
Imalka1 <31820122+Imalka1@users.noreply.github.com>
2017-09-13 23:39:56 +0530

    KingQueen

--- js/screenSizeController.js
+++ js/screenSizeController.js
@@ -45,6 +45,9 @@ function setMediaSize(outerPanelSize,boardSize,elementSize,pos1,pos2,imgSize) {
 function mediaSize() {
     if(window.matchMedia("(orientation: portrait)").matches){
         $("body").css("background-image","url('images/Chess-king1.jpg')");
+        $("body").css("background-repeat","no-repeat");
+        $("body").css("background-attachment","fixed");
+        $("body").css("background-size","cover");
         //$("body").css("background-color","#4A3301");
         //window.alert("Please Switch Your Screen into Landscape Mode For Better Experience");
     }else if(window.matchMedia("(orientation: landscape)").matches){
@@ -62,7 +65,7 @@ function mediaSize() {
         setMainHoldPanel("875px","350px","1100px");
         setElementHoldPanel(1, "0px", "0px","175px", "174px", divOuterTop[7], "Black","30px");
         setElementHoldPanel(2, "175px", "0px","175px", "174px", divOuterTop[7], "White","30px");
-        setTextSize("55px");
+        setTextSize("45px");
         setFooter("1470px");
     }else if(window.matchMedia("(min-width: 720px)").matches) {
         divPos=[0,75,150,225,300,375,450,525];
@@ -72,23 +75,14 @@ function mediaSize() {
         setMainHoldPanel("750px","300px","950px");
         setElementHoldPanel(1, "0px", "0px","150px", "149px", divOuterTop[7], "Black","20px");
         setElementHoldPanel(2, "150px", "0px","150px", "149px", divOuterTop[7], "White","20px");
-        setTextSize("45px");
+        setTextSize("35px");
         setFooter("1300px");
-    }else if(window.matchMedia("(min-width: 720px)").matches){
-        divPos=[0,75,150,225,300,375,450,525];
-        divOuterTop=[75,150,225,300,375,450,525,600];
-        divOuterLeft=[0,75,150,225,300,375,450,525,600,675];
-        setMediaSize("475px","300px","75px",divOuterLeft[0],divOuterLeft[9],"67.5px");
-        setElementHoldPanel(1, "250px", "0px", "149px", "600px", divPos[1], 162.5, "Black");
-        setElementHoldPanel(2, "250px", "1300px", "149px", "600px", divPos[1], 162.5, "White");
-        setTextSize("30px");
-        setFooter("1100px");
     }else if(window.matchMedia("(min-width: 520px)").matches){
-        divPos=[0,62.5,69,103.5,138,172.5,207,241.5];
+        /*divPos=[0,62.5,69,103.5,138,172.5,207,241.5];
         divOuterTop=[34.5,69,103.5,138,172.5,207,241.5,276];
         divOuterLeft=[0,34.5,69,103.5,138,172.5,207,241.5,276,310.5];
         setMediaSize("276px","100px","34.5px",divOuterLeft[0],divOuterLeft[9],"27px");
-        setTextSize("20px");
+        setTextSize("20px");*/
     }else if(window.matchMedia("(min-width: 420px)").matches){
         setTextSize("100px");
     }else if(window.matchMedia("(min-width: 320px)").matches){
